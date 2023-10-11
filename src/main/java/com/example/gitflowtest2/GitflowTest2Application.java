3486fa971fbd3812fe8408ea977b022b343a36d8
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-10-11 10:39:45 +0900

    Initial commit

---
+++ src/main/java/com/example/gitflowtest2/GitflowTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.gitflowtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitflowTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitflowtest2/GitflowTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitflowtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
